948b6e87dfd69e38663f5c803fd8c36538e0b1c4
Hnkee Ndersson <hnkee.ndersson@gmail.com>
2019-09-13 06:55:22 +0200

    Added Filter

---
+++ FilteredHead.cs
@@ -0,0 +1,39 @@
+﻿using Headpose.NET.Filters;
+
+namespace Headpose.NET
+{
+    public class FilteredHead : IHead
+    {
+        private IHead _head;
+        public OneEuroFilter PosXFilter;
+        public OneEuroFilter PosYFilter;
+        public OneEuroFilter PosZFilter;
+        public OneEuroFilter RotXFilter;
+        public OneEuroFilter RotYFilter;
+        public OneEuroFilter RotZFilter;
+        public double Rate;
+        public FilteredHead(IHead head, double minCutOff = 1, double beta = 0.001d, double rate = 100)
+        {
+            _head = head;
+            Rate = rate;
+            PosXFilter = new OneEuroFilter(minCutOff, beta);
+            PosYFilter = new OneEuroFilter(minCutOff, beta);
+            PosZFilter = new OneEuroFilter(minCutOff, beta);
+            RotXFilter = new OneEuroFilter(minCutOff, beta);
+            RotYFilter = new OneEuroFilter(minCutOff, beta);
+            RotZFilter = new OneEuroFilter(minCutOff, beta);
+        }
+
+        public float PositionX => (float) PosXFilter.Filter(_head.PositionX, Rate);
+
+        public float PositionY => (float) PosYFilter.Filter(_head.PositionX, Rate);
+
+        public float PositionZ => (float) PosZFilter.Filter(_head.PositionX, Rate);
+
+        public float RotationX => (float) RotXFilter.Filter(_head.PositionX, Rate);
+
+        public float RotationY => (float) RotYFilter.Filter(_head.PositionX, Rate);
+
+        public float RotationZ => (float) RotZFilter.Filter(_head.PositionX, Rate);
+    }
+}
